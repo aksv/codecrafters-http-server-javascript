93e2c961a9b467e15b20ec641fba849c328b75fa
Aleksei Alekseev <alexeev.mail@gmail.com>
2025-10-27 20:15:32 +0100

    Add simple response

--- app/main.js
+++ app/main.js
@@ -1,9 +1,19 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+// TODO: move to consts
+const CRLF = '\r\n';
+
+// TODO: move to config
+const protocolVersion = '1.1';
+
+// TODO: move to utils module
+function makeResponse(version, code, reasonPhrase) {
+  return `HTTP/${version} ${code} ${reasonPhrase}${CRLF}${CRLF}`;
+}
 
 const server = net.createServer((socket) => {
+  const response = makeResponse(protocolVersion, 200, 'OK');
+  socket.write(response);
   socket.on("close", () => {
     socket.end();
   });
